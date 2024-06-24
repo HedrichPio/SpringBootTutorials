afceca410a3c38d295d87b486290f84f76d3fa59
HedrichPio <mhpiofernando1@gmail.com>
2024-06-24 22:35:38 +0200

    added springboot project, created first RestApi

---
+++ learn-spring-boot/src/main/java/com/springboot/hedrich/learn_spring_boot/CourseController.java
@@ -0,0 +1,20 @@
+package com.springboot.hedrich.learn_spring_boot;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+public class CourseController {
+
+		@RequestMapping("/courses")
+		public List<Course> retrieveAllCourses(){
+			return Arrays.asList(
+					new Course(1,"Learn Swift","Hedrich"),
+					new Course (2,"Learn Java","Hedrich")
+					);
+		}
+}
