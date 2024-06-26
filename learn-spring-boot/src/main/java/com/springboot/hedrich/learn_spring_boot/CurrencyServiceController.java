342ac8664cb872b125d8e01ec4659638da080696
HedrichPio <mhpiofernando1@gmail.com>
2024-06-26 23:32:00 +0200

    Used profiles for multiple environment configurations and ConfigurationProperties

---
+++ learn-spring-boot/src/main/java/com/springboot/hedrich/learn_spring_boot/CurrencyServiceController.java
@@ -0,0 +1,20 @@
+package com.springboot.hedrich.learn_spring_boot;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class CurrencyServiceController {
+
+	@Autowired
+	private CurrencyServiceConfiguration currencyServiceConfiguration;
+	
+	@RequestMapping("/currencyService")
+	public CurrencyServiceConfiguration retrieveAllCourses(){
+		return currencyServiceConfiguration;
+	}
+}
