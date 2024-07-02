1f91f32118de0a4960d9ef17a888086cdecc9128
HedrichPio <mhpiofernando1@gmail.com>
2024-07-02 23:54:42 +0200

    added findById query, utilised BeanPropertyRowMapper to convert the query result to Bean using Course class.

--- learn-jpa-and-hibernate/src/main/java/com/hedrich/springboot/learn_jpa_and_hibernate/course/jdbc/CourseJdbcRepository.java
+++ learn-jpa-and-hibernate/src/main/java/com/hedrich/springboot/learn_jpa_and_hibernate/course/jdbc/CourseJdbcRepository.java
@@ -1,6 +1,7 @@
 package com.hedrich.springboot.learn_jpa_and_hibernate.course.jdbc;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
@@ -16,19 +17,26 @@ public class CourseJdbcRepository {
 				insert into course (id, name, author)
 				values(?,?,?);
 			""";
-	
+
 	private static String DELETE_QUERY = """
-			delete from course where id = ?;
-		""";
+				delete from course where id = ?;
+			""";
+
+	private static String SELECT_QUERY = """
+				select * from course where id = ?;
+			""";
 
 	public void insert(Course course) {
 		springJdbcTemplate.update(INSERT_QUERY, course.getId(), course.getName(), course.getAuthor());
 	}
-	
-	
+
 	public void deleteById(long id) {
 		springJdbcTemplate.update(DELETE_QUERY, id);
 	}
-	
+
+	public Course findById(long id) {
+		// ResultSet -> Bean done using Row Mapper
+		return springJdbcTemplate.queryForObject(SELECT_QUERY, new BeanPropertyRowMapper<>(Course.class),id);
+	}
 
 }
